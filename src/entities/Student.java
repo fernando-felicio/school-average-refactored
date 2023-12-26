d8c9f9d4531622c1056ed476046b3bed04ebe812
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-12-26 13:17:10 -0300

    Criando atributos e métodos da classe Student

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -7,5 +7,20 @@ public class Student {
 	public double secondemester;
 	public double thirdSemester;
 	
+	public double annualAverage() {
+		return this.firstSemester + this.secondemester + this.secondemester;
+	}
+	
+	public void showResult() {
+		if (annualAverage() >= 60) {
+			System.out.println("PASS");
+		}
+		else {
+			System.out.println("FAILED");
+			double missingPoints = 100 - annualAverage();
+			System.out.printf("Missing %.2f Points", missingPoints);
+		}
+	}
+	
 
 }
